8942d9cfc30f9cd3cd30f9c0a19cf4b4e48066c4
SnuuDD <mj.jesusj@gmail.com>
2022-05-16 17:08:50 -0500

    CRUD de Permisos, Roles, Users con edicion de registros partiendo de JSON terminados

--- boleteria-ldaw_fj22-backend/database/migrations/2022_05_14_003512_create_users_table.php
+++ boleteria-ldaw_fj22-backend/database/migrations/2022_05_14_003512_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,14 +16,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained()->onDelete("CASCADE");
-            $table->string('name',50);
-            $table->string('tel_number',10);
-            $table->string('email',40);
-            $table->string('password',30);
-            $table->string('type_user',10);
-            $table->boolean('active');
-            $table->timestamps();
+            $table->integer('role_id')->nullable()->default(0);
+            $table->string('name',50)->nullable(false);
+            $table->string('tel_number',10)->nullable(false);
+            $table->string('email',80)->nullable(false);
+            $table->string('password',30)->nullable(false);
+            $table->string('type_user',10)->nullable(false);
+            $table->boolean('active')->default(1);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
--- boleteria-ldaw_fj22-backend/routes/web.php
+++ boleteria-ldaw_fj22-backend/routes/web.php
@@ -10,6 +10,8 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\_TestController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
 
 
 /*--------------------------------------------------------------------------
@@ -18,7 +20,9 @@ use App\Http\Controllers\EventController;
 
 Route::resources(['categories' => CategoryController::class]);
 Route::resources(['events' => EventController::class]);
-Route::resource('users',UserController::class);
+Route::resources(['permissions' => PermissionController::class]);
+Route::resources(['roles' => RoleController::class]);
+Route::resources(['users' => UserController::class]);
 
 
 /* TEST ROUTES */
